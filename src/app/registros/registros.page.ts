f3ab97f6ecb6a8ca6715689b621cd8096ae90713
Manuel Gaona <81531@alumnos.utleon.edu.mx>
2025-02-12 17:43:18 -0600

    pageRegistros

---
+++ src/app/registros/registros.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-registros',
+  templateUrl: './registros.page.html',
+  styleUrls: ['./registros.page.scss'],
+})
+export class RegistrosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
